469aa2b6a9c279c3497759ae1ce8b531535c9345
Katherin Palacio <katherin.palacio01@uceva.edu.co>
2026-03-13 09:11:51 -0500

    chore: corregir estructura de modulos del backend

--- backend/project-analytics/src/main/java/com/uceva/project_analytics/ProjectAnalyticsApplication.java
+++ backend/project-analytics/src/main/java/com/uceva/project_analytics/ProjectAnalyticsApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjectAnalyticsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjectAnalyticsApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectAnalyticsApplication.class, args);
+    }
 }
