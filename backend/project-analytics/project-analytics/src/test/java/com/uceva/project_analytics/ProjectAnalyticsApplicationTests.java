f3fbc36919bc79f79da3b79b2109fa0894fd847a
Katherin Palacio <katherin.palacio01@uceva.edu.co>
2026-03-13 08:51:47 -0500

    feat: inicializar backend Spring Boot project-analytics

---
+++ backend/project-analytics/project-analytics/src/test/java/com/uceva/project_analytics/ProjectAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uceva.project_analytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
